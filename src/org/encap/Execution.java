760eeac36775e69ff4bbc31f58d19c4a9f6f591d
Subhashree1997-ux <subamuki1997@gmail.com>
2023-09-25 08:43:25 +0530

    done1

--- src/org/encap/Execution.java
+++ src/org/encap/Execution.java
@@ -41,6 +41,7 @@ public class Execution {
 		for(EncapLearnPOJO s:si) {
 			System.out.println(s.getCustomerId());
 			 System.out.println(s.getPassword());
+			 System.out.println("Shree");
 		}
 		
 		System.out.println("----Map------"); 
